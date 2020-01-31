9e0e4850c5e3c0bc0a708430f8d011194eff5450
Thomas Christlieb <thomaschristlieb@hotmail.com>
2020-01-31 22:23:10 +0100

    Prepare moving car

--- Scenes/PlayerCar.gd
+++ Scenes/PlayerCar.gd
@@ -1,16 +1,21 @@
 extends Area2D
 
+export (int) var speed = 200
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var velocity = Vector2()
 
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed('right'):
+		velocity.x += 1
+	if Input.is_action_pressed('left'):
+		velocity.x -= 1
+	if Input.is_action_pressed('down'):
+		velocity.y += 1
+	if Input.is_action_pressed('up'):
+		velocity.y -= 1
+	velocity = velocity.normalized() * speed
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	get_input()
+	velocity = move_and_slide(velocity)
