aa1577ee6f24dee010edb2e23d8157d9cdce707c
Thomas Christlieb <thomaschristlieb@hotmail.com>
2020-01-31 22:45:52 +0100

    Make car move like pacman

--- Scenes/PlayerCar/PlayerCar.gd
+++ Scenes/PlayerCar/PlayerCar.gd
@@ -5,16 +5,29 @@ export (int) var speed = 200
 var velocity = Vector2()
 
 func get_input():
-	velocity = Vector2()
 	if Input.is_action_pressed('right'):
+		velocity = Vector2()
 		velocity.x += 1
 	if Input.is_action_pressed('left'):
+		velocity = Vector2()
 		velocity.x -= 1
 	if Input.is_action_pressed('down'):
+		velocity = Vector2()
 		velocity.y += 1
 	if Input.is_action_pressed('up'):
+		velocity = Vector2()
 		velocity.y -= 1
 	velocity = velocity.normalized() * speed
+	
+	if position.x > get_viewport().size.x:
+		position.x = 0
+	if position.y > get_viewport().size.y:
+		position.y = 0
+	if position.x < 0:
+		position.x = get_viewport().size.x
+	if position.y < 0:
+		position.y = get_viewport().size.y
+
 
 func _physics_process(delta):
 	get_input()
