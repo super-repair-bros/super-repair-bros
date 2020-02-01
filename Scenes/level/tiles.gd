397b77c8d8b79499dad66e7815ed46441bbe4ddb
Dario <ddtyphos@gmail.com>
2020-02-01 09:16:09 +0100

    camera und projektanpassung

--- Scenes/level/tiles.gd
+++ Scenes/level/tiles.gd
@@ -1,10 +1,10 @@
 extends TileMap
 
-export var levelwidht = 48
-export var levelheight = 48
+#export var levelwidht = 48
+#export var levelheight = 48
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	for lvlx in range(0, levelwidht):
-		for lvly in range(0, levelheight):
-			set_cell(lvlx, lvly, 0);
+#func _ready():
+#	for lvlx in range(0, 1200):
+#		for lvly in range(0, 800):
+#			set_cell(lvlx, lvly, 0);
