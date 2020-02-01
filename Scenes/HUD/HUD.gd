1491eedaec73ee4e0db62ec49a13185d2ab41a3b
Christoph Lahner <christoph.lahner@online.de>
2020-02-01 12:43:29 +0100

    Prepare HUD

---
+++ Scenes/HUD/HUD.gd
@@ -0,0 +1,31 @@
+extends Node
+
+export var timeInSec = 20;
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	refreshTime()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Timer_timeout():
+	timeInSec = timeInSec - 1;
+	
+	if timeInSec <= 0:
+		emit_signal("GameOver")
+		$"HBoxContainer/Timer".stop()
+	
+	refreshTime()
+
+func refreshTime():
+	$"HBoxContainer/time".text = "Time left: %s" % timeInSec;
---
+++ Scenes/HUD/Timer.gd
@@ -0,0 +1,5 @@
+extends Timer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
