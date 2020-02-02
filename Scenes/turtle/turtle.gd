579e9ce56736034fe4e04359385b149d98df5f6c
Christoph Lahner <christoph.lahner@online.de>
2020-02-02 11:32:02 +0100

    Add points for smashing the turtles

--- Scenes/turtle/turtle.gd
+++ Scenes/turtle/turtle.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+signal turtle_smashed
+
 var myturtlelocations = []
 
 func _on_tiles_placeturtle(turtlelocations):
@@ -9,6 +11,7 @@ func _on_tiles_placeturtle(turtlelocations):
 
 
 func place_turtle():
+	randomize()
 	var newpos = myturtlelocations[randi() % myturtlelocations.size()]
 	position.x = newpos[0]
 	position.y = newpos[1]
@@ -17,5 +20,5 @@ func place_turtle():
 
 func _on_turtle_body_entered(body):
 	if body.name == "MyCar":
-		emit_signal("turtlesmashed")
+		emit_signal("turtle_smashed")
 		place_turtle()
