b9150716fbe837635d371987d285bac7a432c64d
Thomas Christlieb <thomaschristlieb@hotmail.com>
2020-02-02 11:00:26 +0100

    More work at turtles

--- Scenes/level/tiles.gd
+++ Scenes/level/tiles.gd
@@ -4,9 +4,9 @@ export var levelwidht = 8
 export var levelheight = 4
 
 var turtlelocations = []
+var tile_size = 24;
 
 signal placeturtle
-signal showturtle
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -32,21 +32,23 @@ func _ready():
 				if randnum == 2:
 					set_cell(lvlx, lvly, 1)
 				if randnum == 3:
-					# This tile has a crosswalk, remember that for our tutrle locations
-					
-					set_cell(lvlx, lvly, 2)
+					# This tile has a crosswalk, remember that for our turtle locations
+					turtlelocations.append(Vector2(lvlx * tile_size, lvly * tile_size))					
+					set_cell(lvlx, lvly, 3)
 				if randnum == 4:
-					set_cell(lvlx, lvly, 5)
+					set_cell(lvlx, lvly, 4)
 			else:
 				# 2 Blocked tiles
 				var randnum = randi() % 2 + 1
 				print(str(randnum))
 				if randnum == 1:
-					set_cell(lvlx, lvly, 3)
+					set_cell(lvlx, lvly, 2)
 				if randnum == 2:
-					set_cell(lvlx, lvly, 4)
+					set_cell(lvlx, lvly, 5)
 	# Set a turtle
-	
+	if turtlelocations:
+		emit_signal("placeturtle", turtlelocations[0])
+		emit_signal("showturtle")
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
