e7067510b615bba23c9e4498b98c9e242ed75d58
Thomas Christlieb <thomaschristlieb@hotmail.com>
2020-02-05 11:33:15 +0100

    Add Highscore Buttons

--- Scenes/TitleScreen/TitleScreen.gd
+++ Scenes/TitleScreen/TitleScreen.gd
@@ -2,30 +2,30 @@ extends Control
 
 
 func _ready():
-	if OS.get_name() == 'HTML5':
-		get_node("MainMenu/HBoxContainer/VBoxContainer/VBoxContainer/ExitLabel").set_visible(false)
-	$MainMenu/HBoxContainer/VBoxContainer/VBoxContainer/NewGameLabel.grab_focus()
+    if OS.get_name() == 'HTML5':
+        get_node("MainMenu/HBoxContainer/VBoxContainer/VBoxContainer/ExitLabel").set_visible(false)
+    $MainMenu/HBoxContainer/VBoxContainer/VBoxContainer/NewGameLabel.grab_focus()
 
 
 func _process(_delta):
-	
-	if $MainMenu/HBoxContainer/VBoxContainer/VBoxContainer/NewGameLabel.is_hovered() == true:
-		$MainMenu/HBoxContainer/VBoxContainer/VBoxContainer/NewGameLabel.grab_focus()
-		
-	if $MainMenu/HBoxContainer/VBoxContainer/VBoxContainer/CreditsLabel.is_hovered() == true:
-		$MainMenu/HBoxContainer/VBoxContainer/VBoxContainer/CreditsLabel.grab_focus()
+    
+    if $MainMenu/HBoxContainer/VBoxContainer/VBoxContainer/NewGameLabel.is_hovered() == true:
+        $MainMenu/HBoxContainer/VBoxContainer/VBoxContainer/NewGameLabel.grab_focus()
+        
+    if $MainMenu/HBoxContainer/VBoxContainer/VBoxContainer/CreditsLabel.is_hovered() == true:
+        $MainMenu/HBoxContainer/VBoxContainer/VBoxContainer/CreditsLabel.grab_focus()
 
 
 func _on_NewGameLabel_pressed():
-	get_tree().change_scene("res://Scenes/level/level.tscn")
+    get_tree().change_scene("res://Scenes/level/level.tscn")
 
-	
+    
 func _on_CreditsLabel_pressed():
-	get_tree().change_scene("res://Scenes/Credits/Credits.tscn")
+    get_tree().change_scene("res://Scenes/Credits/Credits.tscn")
 
 
 func _on_ExitLabel_pressed():
-	get_tree().quit()
+    get_tree().quit()
 
 func _on_HighscoreBtn_pressed():
-	get_tree().change_scene("res://Scenes/Highscore/Highscore.tscn")    
+    get_tree().change_scene("res://Scenes/Highscore/Highscore.tscn")    
